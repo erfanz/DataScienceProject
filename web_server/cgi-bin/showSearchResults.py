5e3c9e50263d6de844b77fc24eb0b668c2426695
erfanz <erfan.zamanian@gmail.com>
2015-05-01 16:36:09 -0400

    Added webserver

---
+++ web_server/cgi-bin/showSearchResults.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python
+
+# Import modules for CGI handling 
+import cgi, cgitb 
+
+# Create instance of FieldStorage 
+form = cgi.FieldStorage() 
+
+# Get data from fields
+searchQuery = form.getvalue('query')
+
+print "Content-type:text/html\r\n\r\n"
+print "<html>"
+print "<head>"
+print "<title>Hello - Second CGI Program</title>"
+print "</head>"
+print "<body>"
+print "<h2>Hello %s</h2>" % (searchQuery)
+print "</body>"
+print "</html>"
