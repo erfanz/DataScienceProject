3d5faf24e56b57a6f32199ad7fc5e3a1e5735c40
Scott Houde <scott_houde@brown.edu>
2015-05-08 14:17:02 -0400

    report and JSON stuff

--- data/IPEDS/Inst_to_JSON.py
+++ data/IPEDS/Inst_to_JSON.py
@@ -19,11 +19,18 @@ def main():
 			# {"type":"Feature","id":"01","geometry":{"type":"Point","coordinates":[-86.766233,33.001471]},"properties":{"name":"Alabama","population":4447100}},
 			
 			row_list = row.split(",")
-			unit = row_list[0]	
+			unit = row_list[0]
+			name = row_list[1]
+			addr = row_list[2]
+			city = row_list[3]
+			state = row_list[4]
+			zip_code = row_list[5]
+			website = row_list[15]
+			price_calc = row_list[19]	
 			lng = row_list[64]
 			lat = row_list[65]
 
-			return_string = '{"type":"Feature","id":"' + unit + '","geometry":{"type":"Point","coordinates":[' + lng + ',' + lat + ']}},'
+			return_string = '{"type":"Feature","id":"' + unit + '","geometry":{"type":"Point","coordinates":[' + lng + ',' + lat + ']}, "properties":{"name":' + name + ',"addr":' + addr + ',"city":' + city + ',"state":' + state + ',"zip_code":"' + zip_code + '","website":' + website + ',"price_calc":' + price_calc + '}},\n'
 			output_file.write(return_string)
 
 	output_file.write(']}')
