9e0351d56811e82626ac3998bba93a61e915713a
Scott Houde <scott_houde@brown.edu>
2015-05-08 16:24:25 -0400

    Added Dept Website and Faculty count to inst JSON

--- data/IPEDS/Inst_to_JSON.py
+++ data/IPEDS/Inst_to_JSON.py
@@ -17,6 +17,8 @@ def main():
 	with open(opts.input, 'rb') as f:
 		for row in f:
 			# {"type":"Feature","id":"01","geometry":{"type":"Point","coordinates":[-86.766233,33.001471]},"properties":{"name":"Alabama","population":4447100}},
+
+			# NOTE:  You will need to strip the header row out of the output
 			
 			row_list = row.split(",")
 			unit = row_list[0]
@@ -30,7 +32,7 @@ def main():
 			lng = row_list[64]
 			lat = row_list[65]
 
-			return_string = '{"type":"Feature","id":"' + unit + '","geometry":{"type":"Point","coordinates":[' + lng + ',' + lat + ']}, "properties":{"name":' + name + ',"addr":' + addr + ',"city":' + city + ',"state":' + state + ',"zip_code":"' + zip_code + '","website":' + website + ',"price_calc":' + price_calc + '}},\n'
+			return_string = '{"type":"Feature","id":"' + unit + '","geometry":{"type":"Point","coordinates":[' + lng + ',' + lat + ']}, "properties":{"name":' + name + ',"addr":' + addr + ',"city":' + city + ',"state":' + state + ',"zip_code":' + zip_code + ',"website":' + website + ',"price_calc":' + price_calc + '}},\n'
 			output_file.write(return_string)
 
 	output_file.write(']}')
