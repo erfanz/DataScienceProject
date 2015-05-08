7ef067dabb7cf80102be7a396caadec5a27260ec
yeounoh <yeounohster@gmail.com>
2015-05-08 18:52:23 -0400

    final report updated, indexed_faculty.csv updated

--- code/data_cleaning/index_faculty.py
+++ code/data_cleaning/index_faculty.py
@@ -23,38 +23,55 @@ def index_faculty(faculty_file,corpus_file):
 	corpus = {}
 	with open(corpus_file,'r') as f:
 		for line in f:
+			line = line.decode('utf-8','replace')
 			prof = line.lower().split(',')
 			if len(prof) < 3:
 				continue
-			corpus[prof[1].split()[1]] = (prof[1].split()[0],prof[0])
-	
+			corpus[prof[1]] = prof[0]
+			print prof[1] #debug	
+
 	faculty_index = {}
 	with open(faculty_file,'r') as f, open('faculty_indexed.csv','w') as fi:
 		for line in f:
+			line = line.decode('utf-8','replace')
 			prof = line.lower().split(',')
 			if len(prof) < 3:
 				continue
-			if len(prof[0].split()) < 2:
+			if prof[0].split()[0] == 'name':
 				continue
+			print prof[0].split() #debug
+
 			first_name = prof[0].split()[0]
-			last_name = prof[0].split()[1]
-			if last_name in corpus:
-				first_id = corpus[last_name]
-				if edit_dist(first_id[0],first_name) < 3:
-					faculty_index[first_id[1]] = line
+			last_name = prof[0].split()[-1]
+			full_name = first_name + " " + last_name
+			
+			id = -1
+			not_identified = 1;
+			if full_name in corpus:
+				id = corpus[full_name]
+				faculty_index[id] = line
+				not_identified = 0;
 			else:
-				#not_identified = 1;
-				for l in corpus.keys():
-					if edit_dist(l,last_name) < 3:
-						first_id = corpus[l]
-						if edit_dist(first_id[0],first_name) < 3:
-							faculty_index[first_id[1]] = line
-							not_identified = 0;
+				for k_full_name in corpus.keys(): #full name
+					k_last_name = k_full_name.split()[1]
+					k_first_name = k_full_name.split()[0]
+					if edit_dist(k_last_name,last_name) < 3:
+						id = corpus[k_full_name]				
+
+						if edit_dist(k_first_name,first_name) < 3:
+							not_identified = 0
+							faculty_index[id] = line
 							break
-				#if not_identified == 1:
+
+				if not_identified == 1:
+					if id == -1:
+						print '[Non-identifiable] ' + first_name,last_name
+					else:
+						faculty_index[id] = line
 					#faculty_index[line.split(',')[0]] = line;
+
 		for k, v in faculty_index.items():
-			fi.write(k + "," + v)
+			fi.write(k.encode('ascii','ignore') + "," + v.encode('ascii','ignore'))
 
 
 def main():
