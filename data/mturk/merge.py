cd9b9de0cafb39cc0bb0cf1829ab794a9a381993
John No <johannes.novotny@gmail.com>
2015-05-08 19:21:49 -0400

    Added tooltips to frontpage

--- data/mturk/merge.py
+++ data/mturk/merge.py
@@ -8,8 +8,11 @@ mFile = open(('merged.csv'), 'w')
 def merge_url (urlA, urlB):
 	stripA = urlA.replace('http://','').replace('https://','').rstrip('/')
 	stripB = urlB.replace('http://','').replace('https://','').rstrip('/')
-	if stripA != stripB:
-		print(urlA + ' -- ' + urlB)
+	if stripA.startswith('http
+	
+	
+	#if stripA != stripB:
+	#	print(urlA + ' -- ' + urlB)
 		
 
 filereader = csv.reader(open(('batch_results.csv'),'r'))
